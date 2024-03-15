24b922968fb7f935e89650d2b218cdbdd94d3b64
Medhashis_Maiti <medhashis000@gmail.com>
2024-03-15 17:30:04 +0530

    handle the price format in different currency in the cart

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// format price for the price section in the cart
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: "USD" | "EUR" | "GBP" | "BDT";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {},
+) {
+  // Intl.Num... is the way to re present numbers see js docs
+  // compact is a value for num format
+  // passing the default values in the options
+  const { currency = "USD", notation = "compact" } = options;
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+  // the Intl takes the notation currency ; it is their default prop
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
+}
+// TODO  railways
