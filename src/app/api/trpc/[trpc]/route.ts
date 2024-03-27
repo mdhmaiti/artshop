7563994270bdd96dc0d6426e658bb162a6def234
Medhashis_Maiti <medhashis000@gmail.com>
2024-03-27 18:31:33 +0530

    next js listens and forward to the trpc

---
+++ src/app/api/trpc/[trpc]/route.ts
@@ -0,0 +1,21 @@
+// this is the next js dynamic route that listens to any rquest with api/trpc/anything
+// it listens to the request then forward to the trpc backend that is the index.ts in trpc.
+// the appRouter in the index is the backend
+//
+// how to forward it ? by using the fetch request handler in the trpc
+// then pass the end point as the api/trpc and router as the appRouter
+
+import { appRouter } from "@/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+
+const handler = (req: Request) => {
+  // it also takes createContext parameter; it essentially helps to manupulate the req and res
+  fetchRequestHandler({
+    endpoint: "/api/trpc",
+    req,
+    router: appRouter,
+    createContext: () => ({}),
+  });
+};
+
+export { handler as GET, handler as POST };
