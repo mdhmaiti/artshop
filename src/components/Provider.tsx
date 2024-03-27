5796c9b985e93c6a7e82bf279a845b30ac5d5729
Medhashis_Maiti <medhashis000@gmail.com>
2024-03-27 18:32:27 +0530

    integration of trpc and react query basic trpc set up

---
+++ src/components/Provider.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { PropsWithChildren, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { trpc } from "@/trpc/client";
+import { httpBatchLink } from "@trpc/client";
+
+// the credentials : " include" means it allows tbe authentication and the cookies to pass
+// PropsWithChildren is same as the {children : ReactNode} juse a short hand
+const Providers = ({ children }: PropsWithChildren) => {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/trpc`,
+          fetch(url, options) {
+            return fetch(url, {
+              ...options,
+              credentials: "include",
+            });
+          },
+        }),
+      ],
+    })
+  );
+
+  // here trpc is a wrapper on the react query to provide effective communication between the types
+  return (
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </trpc.Provider>
+  );
+};
+
+export default Providers;
