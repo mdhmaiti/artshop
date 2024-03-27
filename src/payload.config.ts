46927e51ea9ddf5fb4ef832a66eb8ded7aa5b6c8
Medhashis_Maiti <medhashis000@gmail.com>
2024-03-27 18:42:50 +0530

    payload confiuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,13 +3,14 @@ import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import path from "path";
-// import { Users } from "./collections/Users";
+import { Users } from "./collections/Users";
 import dotenv from "dotenv";
 // import { Products } from "./collections/Products/Products";
 // import { Media } from "./collections/Media";
 // import { ProductFiles } from "./collections/ProductFile";
 // import { Orders } from "./collections/Orders";
 
+// this dot env is requied other wise the process.env does not act here; i do not know why.
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 });
@@ -17,12 +18,13 @@ dotenv.config({
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
   // collections: [Users, Products, Media, ProductFiles, Orders],
-  collections: [],
+  collections: [Users],
   routes: {
     admin: "/sell",
   },
   admin: {
-    // user: "users",
+    // here the user is assigned a  custom collection called the users defined in the collections folder.
+    user: "users",
     bundler: webpackBundler(),
     meta: {
       titleSuffix: "- DigitalHippo",
@@ -38,6 +40,9 @@ export default buildConfig({
     url: process.env.MONGODB_URL!,
   }),
   typescript: {
+    // this is the befit of the cms
+    // it automatically create the types using the script in the package.json
+    // it helps to prevent the errors
     outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
 });
