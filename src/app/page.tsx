ac40b1735b64f7f1f131857223c346bd03003665
Medhashis_Maiti <medhashis000@gmail.com>
2024-04-02 17:15:49 +0530

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,32 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import ProductReel from "@/components/ProductReel";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 
+const perks = [
+  {
+    name: "Instant Delivery",
+    Icon: ArrowDownToLine,
+    description:
+      "Get your assets delivered to your email in seconds and download them right away.",
+  },
+  {
+    name: "Guaranteed Quality",
+    Icon: CheckCircle,
+    description:
+      "Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.",
+  },
+  {
+    name: "For the Planet",
+    Icon: Leaf,
+    description:
+      "We've pledged 1% of sales to the preservation and restoration of the natural environment.",
+  },
+];
+
 export default function Home() {
-  const perks = [
-    {
-      name: "Instant Delivery",
-      Icon: ArrowDownToLine,
-      description:
-        "Get your assets delivered to your email in seconds and download them right away.",
-    },
-    {
-      name: "Guaranteed Quality",
-      Icon: CheckCircle,
-      description:
-        "Every asset on our platform is verified by our team to ensure our highest quality standards. Not happy? We offer a 30-day refund guarantee.",
-    },
-    {
-      name: "For the Planet",
-      Icon: Leaf,
-      description:
-        "We've pledged 1% of sales to the preservation and restoration of the natural environment.",
-    },
-  ];
   return (
-    // this MaxWidthWrapper defines the top section for the pages
     <>
       <MaxWidthWrapper>
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
@@ -45,8 +45,14 @@ export default function Home() {
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-        {/* TODO list products*/}
+
+        <ProductReel
+          query={{ sort: "desc", limit: 4 }}
+          href="/products?sort=recent"
+          title="Brand new"
+        />
       </MaxWidthWrapper>
+
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
           <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
