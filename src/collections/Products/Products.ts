973a7797c56d5ecda18a8171b92a01e547884583
Medhashis_Maiti <medhashis000@gmail.com>
2024-04-02 17:17:29 +0530

    product schemas where the stripe and product relation are defined

---
+++ src/collections/Products/Products.ts
@@ -0,0 +1,283 @@
+import {
+  AfterChangeHook,
+  BeforeChangeHook,
+} from "payload/dist/collections/config/types";
+import { PRODUCT_CATEGORIES } from "../../config";
+import { Access, CollectionConfig } from "payload/types";
+import { Product, User } from "../../payload-types";
+import { stripe } from "../../lib/stripe";
+
+const addUser: BeforeChangeHook<Product> = async ({ req, data }) => {
+  const user = req.user;
+
+  return { ...data, user: user.id };
+};
+
+const syncUser: AfterChangeHook<Product> = async ({ req, doc }) => {
+  const fullUser = await req.payload.findByID({
+    collection: "users",
+    id: req.user.id,
+  });
+
+  if (fullUser && typeof fullUser === "object") {
+    const { products } = fullUser;
+
+    const allIDs = [
+      ...(products?.map((product) =>
+        typeof product === "object" ? product.id : product
+      ) || []),
+    ];
+
+    const createdProductIDs = allIDs.filter(
+      (id, index) => allIDs.indexOf(id) === index
+    );
+
+    const dataToUpdate = [...createdProductIDs, doc.id];
+
+    await req.payload.update({
+      collection: "users",
+      id: fullUser.id,
+      data: {
+        products: dataToUpdate,
+      },
+    });
+  }
+};
+
+const isAdminOrHasAccess =
+  (): Access =>
+  ({ req: { user: _user } }) => {
+    const user = _user as User | undefined;
+
+    if (!user) return false;
+    if (user.role === "admin") return true;
+
+    const userProductIDs = (user.products || []).reduce<Array<string>>(
+      (acc, product) => {
+        if (!product) return acc;
+        if (typeof product === "string") {
+          acc.push(product);
+        } else {
+          acc.push(product.id);
+        }
+
+        return acc;
+      },
+      []
+    );
+
+    return {
+      id: {
+        in: userProductIDs,
+      },
+    };
+  };
+
+export const Products: CollectionConfig = {
+  // slug : mode mandatory field
+  // to make things visible add products in the payload config.ts under collection
+  slug: "products",
+  admin: {
+    useAsTitle: "name",
+  },
+  //
+  //giving access to the products
+  access: {
+    read: isAdminOrHasAccess(),
+    update: isAdminOrHasAccess(),
+    delete: isAdminOrHasAccess(),
+  },
+  hooks: {
+    afterChange: [syncUser],
+    // this hook is responsible to initiate a new create /  update product in the stripe
+    beforeChange: [
+      addUser,
+      async (args) => {
+        if (args.operation === "create") {
+          const data = args.data as Product;
+
+          const createdProduct = await stripe.products.create({
+            name: data.name,
+            default_price_data: {
+              currency: "USD",
+              unit_amount: Math.round(data.price * 100),
+            },
+          });
+
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
+          };
+
+          return updated;
+        } else if (args.operation === "update") {
+          const data = args.data as Product;
+
+          const updatedProduct = await stripe.products.update(data.stripeId!, {
+            name: data.name,
+            default_price: data.priceId!,
+          });
+
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          };
+
+          return updated;
+        }
+      },
+    ],
+  },
+  fields: [
+    // each product has a user who uploaded it; and the user should be related to the users collection; i,e authenticated user;
+    // 'required'-> each products must have a user;
+    // 'hasMany'-> false means each product should only be uploaded by a single user
+    // admin conditon false means hide my user name from the admin dash board for the products ; as it is  useless here as i already know it is my dash board;
+    {
+      name: "user",
+      type: "relationship",
+      relationTo: "users",
+      required: true,
+      hasMany: false,
+      admin: {
+        condition: () => false,
+      },
+    },
+    //
+    // name : name of the product
+    // lable : the name which will be visible in the dashboard
+    // it will be a text and it is a mandatory fields
+    {
+      name: "name",
+      label: "Name",
+      type: "text",
+      required: true,
+    },
+    // product description/ details optional
+    //
+    {
+      name: "description",
+      type: "textarea",
+      label: "Product details",
+    },
+    // product price numbert type and mandatory fields
+    {
+      name: "price",
+      label: "Price in USD",
+      min: 0,
+      max: 1000,
+      type: "number",
+      required: true,
+    },
+    // the product categories is already defined in the config, just map them over to show in the dash board
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    //
+    //
+    //this is the actual product that the user is paying
+    //'hasMany' false -> each product have a single product file;
+    //but if i want to have multiple product files then ; hasMany-> true will do the job
+    {
+      name: "product_files",
+      label: "Product file(s)",
+      type: "relationship",
+      required: true,
+      relationTo: "product_files",
+      hasMany: false,
+    },
+    //
+    //
+    //after the user listed their products it should be verified by the admin if it is allowed to sale or not
+    // and it must have only the three options i,e, pendin/approved / denied
+    {
+      name: "approvedForSale",
+      label: "Product Status",
+      type: "select",
+      defaultValue: "pending",
+      access: {
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+      options: [
+        {
+          label: "Pending verification",
+          value: "pending",
+        },
+        {
+          label: "Approved",
+          value: "approved",
+        },
+        {
+          label: "Denied",
+          value: "denied",
+        },
+      ],
+    },
+    //
+    //
+    //payment section for the products;
+    //priceId is the id which will be associated with the type and we do not want to give access to anybody
+    //we also donot want the admin to know the pdt id only the sustem should know;
+    {
+      name: "priceId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    // stripe configuration
+
+    {
+      name: "stripeId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+
+    //
+    //
+    //this is the product images that the user will see;
+    //this is kind of the thumb nail;
+    //the thumbnail have the multiple images such that the user can scroll through different images
+    //the user in the dashboard can upload max upto 4 images;
+    {
+      name: "images",
+      type: "array",
+      label: "Product images",
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media",
+          required: true,
+        },
+      ],
+    },
+  ],
+};
